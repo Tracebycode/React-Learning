e6c03afe30a88d050f84a9b48e3e2e71e585f9e6
Abhishek Barik <abhishekbarik974@gmail.com>
2025-03-19 22:34:39 +0530

    commit on the Passward generator--end code

--- Passward Generator/src/App.jsx
+++ Passward Generator/src/App.jsx
@@ -1,25 +1,57 @@
-import { useState } from 'react'
+import { useState,useCallback, useEffect ,useRef} from 'react'
 import './App.css'
 
 function App() {
   const [length,setlength]= useState(8);
-  const [number,setnumber]=useState(false)
-  const [char,setchar] = useState(false);
+  const [numberallowed,setnumberallowed]=useState(false)
+  const [charallowed,setcharallowed] = useState(false);
   const[passward,setpassward]=useState("")
+  const passwardref=useRef(null)
+
+
+  const passwardgenerator=useCallback(()=>{
+    let pass="";
+    let string ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberallowed) string+="1234567890";
+    if(charallowed) string+= "!@#$%^&*()_+{}:<>?[];'.,/"
+
+    for(let i=0;i<=length;i++){
+      let char = Math.floor(Math.random()*string.length+1)
+      pass+=string.charAt(char)
+
+    }
+
+    setpassward(pass)
+
+  },[length,numberallowed,charallowed,setpassward])
+
+  const copypasswardtoclip=useCallback(()=>{
+    passwardref.current?.select()
+    window.navigator.clipboard.writeText(passward)
+  },[passward])
+
+  useEffect(()=>{
+    passwardgenerator()
+  },[length,charallowed,numberallowed,passwardgenerator])
+
+
   
   return (
-    <div className=' w-full  max-w-md bg-gray-800 mx-auto text-white  px-5 py-4 my-8 rounded-lg text-2xl'>
+    <div className='   w-full max-w-max bg-gray-800 mx-auto shadow-md px-5 text-amber-600 my-8 rounded-lg text-2xl'>
       <h1 className='text-4xl text-center my-5'>Passward Generator</h1>
       <div className='flex shadow rounded-lg overflow-hidden  mb-8 bg-amber-300'>
         <input
         value={passward}
-        placeholder='passward'
+        placeholder="passward"
         type='text'
         readOnly
-        className='outline-none w-full bg-amber-50 '/>
-        <button className='bg-blue-600 outline-none  shrink-0 px-5 py-0.5 r'>copy</button>
+        ref={passwardref}
+        className='outline-none w-full text-black bg-amber-50'/>
+        <button 
+        onClick={copypasswardtoclip}
+        className='bg-blue-600 cursor-pointer outline-none  shrink-0 px-5 py-0.5  hover:bg-blue-300'>copy</button>
       </div>
-      <div>
+      <div className='flex gap-x-2 '>
         <div className='flex gap-x-1'>
             <input
             type='range'
@@ -30,6 +62,27 @@ function App() {
             onChange={(e)=>{setlength(e.target.value)}}
             />
             <label >Label:{length}</label>
+        </div>
+        <div className='flex items-center  gap-x-1'>
+          <input
+          type='checkbox'
+          defaultChecked={numberallowed}
+          className=''
+          onChange={()=>{
+            setnumberallowed((prev)=>!(prev))
+          }}
+          />
+          <label >Number</label>
+        </div>
+        <div className='flex items-center  gap-x-1'>
+          <input
+          type='checkbox'
+          defaultChecked={charallowed}
+          className=''
+          onChange={()=>{
+            setcharallowed((prev)=>!(prev))
+          }}/>
+          <label >Character</label>
         </div>
       </div>
     </div>
